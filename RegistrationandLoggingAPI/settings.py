de07aee1f232e8daf86d2d1758d066a6fcecf2bd
komalkknk <78220140+komalkknk@users.noreply.github.com>
2021-09-25 21:42:00 +0530

    Update settings.py

--- RegistrationandLoggingAPI/settings.py
+++ RegistrationandLoggingAPI/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')4igxoo19^i5(c))95a(-vu=jidznl7qg&)gpfj_8rvw_9nd+9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
